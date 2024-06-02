cf9b55ba9c4151c0912d09e59e83ae438eac7733
Hoang Ha <SmithMerch2301@gmail.com>
2024-06-02 21:28:33 +0700

    pass stage

--- app/main.rb
+++ app/main.rb
@@ -1,4 +1,5 @@
 require 'zlib'
+require 'digest/sha1'
 
 command = ARGV[0]
 case command
@@ -16,6 +17,18 @@ when "cat-file"
   uncompressed = Zlib::Inflate.inflate(compressed)
   content = uncompressed.split("\0", 2)[1]
   print content
+when "hash-object"
+  file = ARGV[2]
+  uncompressed_data = File.read(file)
+  data = "blob #{uncompressed_data.bytesize}\0" + uncompressed_data
+  sha1 = Digest::SHA1.hexdigest(data)
+  puts sha1
+  compressed_data = Zlib::Deflate.deflate(data)
+  directory, filename = sha1[0..1], sha1[2..-1]
+  path = ".git/objects/#{directory}"
+  Dir.mkdir(path) unless Dir.exist?(path)
+  file_path = "#{path}/#{filename}"
+  File.write(file_path, compressed_data)
 else
   raise RuntimeError.new("Unknown command #{command}")
 end
