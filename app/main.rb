cb086d77848fe7a0d4bcad41de471d85c2211ff6
Hoang Ha <SmithMerch2301@gmail.com>
2024-06-02 18:21:37 +0700

    pass stage

--- app/main.rb
+++ app/main.rb
@@ -1,3 +1,5 @@
+require 'zlib'
+
 command = ARGV[0]
 case command
 when "init"
@@ -6,6 +8,14 @@ when "init"
   Dir.mkdir(".git/refs")
   File.write(".git/HEAD", "ref: refs/heads/main\n")
   puts "Initialized git directory"
+when "cat-file"
+  object = ARGV[2]
+  directory, filename = object[0..1], object[2..-1]
+  path = ".git/objects/#{directory}/#{filename}"
+  compressed = File.read(path)
+  uncompressed = Zlib::Inflate.inflate(compressed)
+  content = uncompressed.split("\0", 2)[1]
+  print content
 else
   raise RuntimeError.new("Unknown command #{command}")
 end
